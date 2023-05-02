287ece5168adb4cc69fa4baa2718c77764b0f58a
iamitsily <ilystx1@gmail.com>
2023-05-02 12:12:14 -0600

    [Yahir]seleccionModificacionPatient

---
+++ patient/service_seleccionPaciente.php
@@ -0,0 +1,33 @@
+<?php 
+
+include '../conexion.php';
+
+//$consulta = "SELECT * FROM dbo.usuario";
+$matricula = $_POST['matricula'];
+
+$consulta = "SELECT * from usuario WHERE matricula = $matricula AND status=1";
+$stmt = sqlsrv_query($conexion, $consulta);
+
+//guarda la consulta 
+if ($stmt === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+$usuarios = array();
+$usuarios['datos'] =array();
+while ($row = sqlsrv_fetch_array($stmt)) {
+    $index['matricula'] =$row['0'];
+    $index['email'] =$row['4']; 
+    $index['telefono'] =$row['5'];
+    $index['password'] = $row['7'];
+    //rol
+    array_push($usuarios['datos'], $index);
+}
+$usuarios["exito"]="1";
+echo json_encode($usuarios);
+
+sqlsrv_free_stmt($stmt); //libera la memoria asociada con el resultado de la consulta SQL
+sqlsrv_close($conexion);
+
+
+?>
