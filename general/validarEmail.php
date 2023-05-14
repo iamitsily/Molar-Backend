6179fee89b7c1a1244c975658a37f46328963b9d
iamitsily <ilystx1@gmail.com>
2023-05-14 01:40:58 -0600

    [yahir]validacionEmailForgetPass

---
+++ general/validarEmail.php
@@ -0,0 +1,31 @@
+<?php 
+
+include '../conexion.php';
+
+$email = $_POST['email'];
+
+$consulta = "SELECT * from usuario WHERE email = '$email' AND status=1";
+$stmt = sqlsrv_query($conexion, $consulta);
+
+if ($stmt === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+$usuarios = array();
+$usuarios['datos'] =array();
+while ($row = sqlsrv_fetch_array($stmt)) {
+    $index['matricula'] =$row['0'];
+    $index['nombre'] =$row['1']; 
+    $index['password'] =$row['7'];
+    $index['rol'] = $row['9'];
+    //rol
+    array_push($usuarios['datos'], $index);
+}
+$usuarios["exito"]="1";
+echo json_encode($usuarios);
+
+sqlsrv_free_stmt($stmt); //libera la memoria asociada con el resultado de la consulta SQL
+sqlsrv_close($conexion);
+
+
+?>
