43a32688d564bf99435c2eb689a7584ee22d049c
iamitsily <ilystx1@gmail.com>
2023-05-29 23:54:21 -0600

    [yahir]iconosPerfil

---
+++ patient/service_editarSexo.php
@@ -0,0 +1,22 @@
+<?php
+
+include '../conexion.php';
+
+$matricula = $_POST['matricula'];
+$sexo = $_POST['sexo'];
+$params = array($sexo);
+
+
+$consulta = "UPDATE usuario SET sexo = (?) WHERE matricula = $matricula";
+$stmt = sqlsrv_prepare($conexion, $consulta, $params);
+
+
+if(sqlsrv_execute($stmt) === false){
+    echo "Modificacion fallida";
+    die(print_r(sqlsrv_errors(), true));
+}else{
+    echo "Modificacion exitosa";
+}
+sqlsrv_close($conexion);
+
+?>
