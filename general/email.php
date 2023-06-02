e407b30dff87f9396e7792c33c6eaea6e24b21f5
iamitsily <ilystx1@gmail.com>
2023-06-02 01:05:42 -0600

    [yahir]fixListarCitasPorCancelar

--- general/email.php
+++ general/email.php
@@ -12,19 +12,17 @@ $destino = $_POST['destino'];
 $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
 
-//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = 0;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'molar.haku@gmail.com';                     //SMTP username
-    $mail->Password   = 'webysycthvywcvlf';                               //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption PHPMailer::ENCRYPTION_SMTPS
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPDebug = 0;                     
+    $mail->isSMTP();                                           
+    $mail->Host       = 'smtp.gmail.com';                     
+    $mail->SMTPAuth   = true;                                   
+    $mail->Username   = 'molar.haku@gmail.com';                    
+    $mail->Password   = 'webysycthvywcvlf';                            
+    $mail->SMTPSecure = 'ssl';          
+    $mail->Port       = 465;
 
     //Recipients
     $mail->setFrom('molar.haku@gmail.com', 'Molar');
