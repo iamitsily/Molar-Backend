c3d9a8df77841cede501474d546c5249386126c9
iamitsily <ilystx1@gmail.com>
2023-06-08 23:11:26 -0600

    [yahit]MolarVersion1.0

---
+++ admin/service_comprobarEmail.php
@@ -0,0 +1,32 @@
+<?php 
+
+include '../conexion.php';
+
+//$consulta = "SELECT * FROM dbo.usuario";
+$email = $_POST['email'];
+
+$consulta = "SELECT matricula FROM usuario WHERE email = '$email' AND status = 1
+UNION
+SELECT matricula FROM medico WHERE email = '$email' AND status = 1";
+$stmt = sqlsrv_query($conexion, $consulta);
+
+//guarda la consulta 
+if ($stmt === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+$usuarios = array();
+$usuarios['datos'] =array();
+while ($row = sqlsrv_fetch_array($stmt)) {
+    $index['matricula'] =$row['0'];
+    //rol
+    array_push($usuarios['datos'], $index);
+}
+$usuarios["exito"]="1";
+echo json_encode($usuarios);
+
+sqlsrv_free_stmt($stmt); //libera la memoria asociada con el resultado de la consulta SQL
+sqlsrv_close($conexion);
+
+
+?>
